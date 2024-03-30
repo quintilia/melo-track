763b905b918af7e6bfcf769e357ae846db8a826a
Michael Babatunde <babamo@farmingdale.edu>
2024-03-30 12:42:55 -0400

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module com.example.melotrack {
+module com.example.mp3pplayer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
 
-
-    opens com.example.melotrack to javafx.fxml;
-    exports com.example.melotrack;
+    opens com.example.mp3pplayer to javafx.fxml;
+    exports com.example.mp3pplayer;
 }
