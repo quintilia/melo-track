d54437149645e53096d2023354964beca182bbdb
Michael Babatunde <babamo@farmingdale.edu>
2024-05-06 18:17:32 -0400

    initial commit - final ui version

---
+++ src/main/java/com/example/mp3pplayer/CreatePlaylistController.java
@@ -0,0 +1,77 @@
+package com.example.mp3pplayer;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CreatePlaylistController {
+
+    @FXML
+    private ListView<String> songPathsListView;
+
+
+    @FXML
+    public void addSongToPlayList(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select MP3 File");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("MP3 files (*.mp3)", "*.mp3");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(null);
+
+
+        if (selectedFiles != null) {
+            for (File file : selectedFiles) {
+                String selectedFile = file.toURI().toString();
+                songPathsListView.getItems().add(selectedFile);
+            }
+        }
+    }
+
+    @FXML
+    public void savePlayList(MouseEvent actionEvent) {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Playlist");
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File selectedFile = fileChooser.showSaveDialog(null);
+
+        if (selectedFile != null) {
+            if (!selectedFile.getName().toLowerCase().endsWith(".txt")) {
+                selectedFile = new File(selectedFile.getAbsolutePath() + ".txt");
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile))) {
+                ObservableList<String> items = songPathsListView.getItems();
+                for (String item : items) {
+                    writer.write(item);
+                    writer.newLine();
+                }
+
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Success");
+                alert.setHeaderText(null);
+                alert.setContentText("Playlist saved successfully.");
+                alert.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Error occurred while saving playlist.", ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
+    }
+
+}
